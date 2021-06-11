f7df8d39e1a3e28273a4150cf8f9b5ec03567804
ishadi123 <ishadi.anuththara22@gmail.com>
2021-06-11 13:21:35 +0530

    Admin Login page editied

--- application/views/admin/admin_login.php
+++ application/views/admin/admin_login.php
@@ -1,3 +1,44 @@
-<?php include 'includes/header_adlogin.php'?>
-	<h2>Admin login page</h2>
-<?php include 'includes/footer.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Online_Notice_Board</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url('assests/css/admin_login_css.css')?>">
+</head>
+<body>
+	
+
+<div class="adlogin_center">
+		
+		
+		<h1>Login</h1>
+					<form method="post">
+				<div class="adlogin_txt_field">
+					<input type="text" required>
+					<span></span>
+						<label>UserName</label>
+				</div>
+
+				<div class="adlogin_txt_field">
+					<input type="password" required>
+					<span></span>
+						<label>Password</label>
+				</div>
+
+				<div>
+					<input type="submit" value="Login">
+						
+		
+				</div>
+		
+			</form>
+	
+	
+	</div>
+
+	</body>
+</html>
--- application/views/home_page.php
+++ application/views/home_page.php
@@ -1,7 +1,6 @@
 <?php include 'partials/header_home.php'?>
 	<h2>Home page</h2>
-	<p>ishadi</p>
-	<p>Rathnayaka</p>
+	
 
 <?php include 'partials/footer.php'?>
 
