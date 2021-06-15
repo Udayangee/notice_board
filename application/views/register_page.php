683814c0fb223672eecf8bce494fcfecc9d6cab5
Udayangee <wkudayangee@gmail.com>
2021-06-15 23:23:25 +0530

    update regitration page.

--- application/views/register_page.php
+++ application/views/register_page.php
@@ -1,2 +1,96 @@
 
-<h1>Regitration form</h1>
+    <head>
+         <meta charset="UTF-8">
+         <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div class="container">
+            <div class="regform">
+                <div><h1>Rgistration Form</h1></div>
+                <form actio="#" id="reg_form">
+                    <div id="reg_entno" class="reg_info" method="post">
+                        <label for="entrollment_no" class="reg_label">Entrollment Number :</label>
+                        <input type="text" placeholder="UWU/XXX/XX/XXX" quid><br><br>
+                    </div>
+                    <div id="reg_name_in" class="reg_info" >
+                        <label for="name_initials" class="reg_label">Name with initials :</label>
+                        <input type="text" placeholder="A.S.PERA" quid><br><br>
+                    </div>
+                    <div id="reg_name_full" class="reg_info">
+                        <label for="full_name" class="reg_label"> Full Name :</label>
+                        <input type="text" placeholder="AMA SHALINI PERA" quid><br><br>
+                    </div>
+                    <div id="reg_gender" class="reg_info">
+                        <label for="gender" class="reg_label">Gender :</label>
+                        <input type="radio" name="gender" value="male">Male<br>
+                        <input type="radio" name="gender" value="female">Female<br><br>
+                    </div>
+                    <div id="reg_dob" class="reg_info">
+                        <label for="dob" class="reg_label">Date of Birth</label>
+                        <input type="date" quid><br><br>
+                    </div>
+                    <div id="reg_faculty" class="reg_info">
+                        <label for="faculty" class="reg_label">Faculty :</label>
+                        <select quid>
+                            <option>Faculty of Animal Science and Export Agricultu</option>
+                            <option>Faculty of Applied Sciences</option>
+                            <option>Faculty of Management</option>
+                            <option>Faculty of Technological Studies</option>
+                        </select><br><br>
+                    </div>
+                    <div id="reg_degree" class="reg_info">
+                        <label for="degree" class="reg_label">Degree Program :</label>
+                        <select quid>
+                            <option>Animal Science Technology Degree Program</option>
+                            <option>Aquatic sources and Technology Degree Program</option>
+                            <option>Export Agricultur Degree Program</option>
+                            <option>Tea Technology and Value Addition Degree Program</option>
+                            <option>Palm and Latex Technology Degree Program</option>
+                            <option>Computer science and Technology Degree Program</option>
+                            <option>Industrial Information Technology Degree Program</option>
+                            <option>Science and Technology Degree Program</option>
+                            <option>Mineral sources and Technology Degree Program</option>
+                            <option>Entepinurship and management Degree Prograam</option>
+                            <option>Hospitality Tourism and events Management Degree Program</option>
+                            <option>Biosystems Technology degree Program</option>
+                            <option>Engineering Technology Degree program</option>
+                        </select><br><br>
+                    </div>
+                    <div id="reg_email" class="reg_info">
+                        <label for="email" class="reg_label">E-mail :</label>
+                        <input type="email" placeholder="xxxxxxxx@std.uwu.ac.lk" quid><br><br>
+                    </div>
+                    <div id="reg_uname" class="reg_info">
+                        <label for="uname" class="reg_label">User Name :</label>
+                        <input type="text" placeholder="ans18001" quid><br><br>
+                    </div>
+                    <div id="reg_pwd" class="reg_info">
+                        <label for="password" class="reg_label">Password :</label>
+                        <input type="" quid><br><br>
+                    </div>
+                    <div id="reg_conpwd" class="reg_info">
+                        <label for="con_password" class="reg_label" >Confirm Password :</label>
+                        <input type="password" quid><br><br>
+                    </div>
+
+                    <div id="reg_pno" class="reg_info">
+                        <label for="phone_no"class="reg_label" >Phone Number :</label>
+                        <input type="tel" quid><br><br>
+                    </div>
+                    <div id="reg_adress" class="reg_info">
+                        <label for="adress" class="reg_label">Adress :</label>
+                        <textarea input type="text" placeholder="PASSARA ROAD, BADULLA" ></textarea><br><br>
+                    </div>
+                    <div id="reg_submit">
+                        <input type="submit" value="Submit">
+                    </div>
+                    <div id="reg_cancel">
+                        <input type="submit" value="Cancel">
+                    </div>
+                </form>   
+
+            </div>
+        </div>   
+    </body>
+
+
