6d0b18c05633dc60ff59c2e67916cad46b39a419
Udayangee <wkudayangee@gmail.com>
2021-06-16 09:52:00 +0530

    Modify super admin create_notice.php

--- application/views/administrators/super_admin/create_notice.php
+++ application/views/administrators/super_admin/create_notice.php
@@ -4,14 +4,52 @@
             <div class="row" id="main" >
                 <div class="col-sm-12 col-md-12 well" id="content">
                     <h1>Create notice!</h1>
-                    <div id="create">
-                    <form action="/action_page.php">
-                        <input type="file" id="myFile" name="filename">
-                         <input type="submit">
-                        </form>
-
-                        </body>
-                    </div>
+                    
+    <div id="super_create_notice">
+        <div id="scn_form">
+            <form id="super_create_notice_form" action="">
+                <div id="scn_upload_notice">
+				<h2 id="scn_un_h2">Upload Notice</h2>
+				<div id="scn_doc">
+					<label id="scn_doc1" class="scn_label">Upload Doc File</label><br>
+					<label id="scn_doc2" class="scn_label">Enter title of Notice</label>
+					<input type="text" value="Title">
+					<input type="file" id="doc_file"><br><br>
+				</div>
+				<div id="scn_pdf">
+					<label id="scn_pdf1" class="scn_label">Upload Pdf File</label><br>
+					<label id="scn_pdf2" class="scn_label">Enter title of Notice</label>
+					<input type="text" value="Title">
+					<input type="file" id="pdf_file"><br><br>
+				</div>
+				<div id="scn_img">
+					<label id="scn_img1" class="scn_label">Upload Notice Image</label><br>
+					<label id="scn_img2" class="scn_label">Enter title of Notice</label>
+					<input type="text" value="Title">
+					<input type="file" id="img_file"><br><br>
+				</div>
+				</div>	
+				
+				<div id="scn_create_notice">
+				<h2 id="scn_un_h2">Create notice</h2>
+					<table id="scn_table"style="border-collapse:collapse; border:1px solid black;">
+						<tr style="border:1px solid black;">
+							<th style="border:1px solid black;" >Title</th>
+							<th>Description</th>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+						</tr>
+					</table><br>
+					<input id="scn_create_notice_submit" type="submit" value="Create Notice"><br><br>
+				</div>
+				<input id="scn_upload_notice_submit"type="submit" value="Upload Notice">
+				<input id="scn_upload_notice_cancel"type="submit" value="Cancel">
+            </form>
+        </div>
+    </div>
+ 
                 </div>
             </div>
             <!-- /.row -->
